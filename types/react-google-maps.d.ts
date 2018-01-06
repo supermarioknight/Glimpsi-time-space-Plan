66559ba0cd250b00eff3ae78f05a81a361747686
supermarioknight <hunter.dev.us@gmail.com>
2018-01-07 09:03:40 +1100

    chore(deps): up deps

--- types/react-google-maps.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'react-google-maps/lib/components/OverlayView' {
-  import { Component, ReactNode } from 'react';
-
-  export interface OverlayViewProps {
-    position: any;
-    getPixelPositionOffset: any;
-  }
-}
